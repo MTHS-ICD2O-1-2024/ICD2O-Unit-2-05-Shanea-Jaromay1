55f068f1cbf5040b55db605f4cb42da81685a3a1
Shanea-Jaromay1 <shanea.jaromay@stu.ocsb.ca>
2025-03-24 17:16:45 +0000

    init

--- js/script.js
+++ js/script.js
@@ -1,12 +1,25 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Shanea Jaromay All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Shanea Jaromay
+// Created on: March 2025
 // This file contains the JS functions for index.html
 
+"use strict"
+
 /**
- * Displays an alert with the message "Hello, World!"
+ * This function calculates salary.
  */
-function myButtonClicked() {
-  alert("Hello, World!")
+function takeHomeSalary () {
+  // input
+  const hoursWorked= parseFloat(document.getElementById('hours-worked').value)
+  const hourlyWage = parseFloat(document.getElementById('hourly-wage').value)
+
+  // process
+  const pay = (hoursWorked * hourlyWage) * (1.00 - 0.18)
+  const taxes = (hoursWorked * hourlyWage) * 0.18
+
+  // output
+  document.getElementById('pay').innerHTML = 
+  `Your pay will be: $${pay.toFixed(2)}
+  <br> The government will take: $${taxes.toFixed(2)}` 
 }
